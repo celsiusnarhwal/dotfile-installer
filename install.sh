4d83f1b7052669a21c17da707fa1c0c1a2b0e7dc
celsius narhwal <hello@celsiusnarhwal.dev>
2026-06-11 15:33:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 if [ ! $(command -v curl) ]; then
     echo "curl must be installed"
     exit 1
@@ -17,6 +16,6 @@ export PATH=$HOME/.local/bin:$PATH
 
 curl -fsSL https://mise.run | sh
 
-export GITHUB_TOKEN=$(mise use -g github:celsiusnarhwal/celty@0.1.0 && $(mise which celty) --client-id Iv23liTL851FJR1MJQIm)
+export GITHUB_TOKEN=$(mise exec github:celsiusnarhwal/celty -- celty --client-id Iv23liTL851FJR1MJQIm)
 
 sh -c "$(curl -fsSL https://get.chezmoi.io)" -- -b ~/.local/bin -- init --apply https://git:${GITHUB_TOKEN}@github.com/celsiusnarhwal/dotfiles
