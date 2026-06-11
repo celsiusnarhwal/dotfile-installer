f02dbbecd60e1d405013cbd96e017d2c7dce4db7
celsius narhwal <hello@celsiusnarhwal.dev>
2026-06-11 06:40:02 -0500

    Initial commit
    Signed-off-by: celsius narhwal <hello@celsiusnarhwal.dev>

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+if ! [[ $(command -v curl) ]]; then
+    echo "curl must be installed"
+    exit 1
+fi
+
+if ! [[ $(command -v git) ]]; then
+    echo "git must be installed"
+    exit 1
+fi
+
+export PATH=$HOME/.local/bin:$PATH
+
+curl -fsSL https://mise.run | sh
+
+export GITHUB_TOKEN=$(mise exec github:celsiusnarhwal/celty -- celty --client-id Iv23liTL851FJR1MJQIm)
+
+sh -c "$(curl -fsSL https://get.chezmoi.io)" -b ~/.local/bin -- init --apply https://git:${GITHUB_TOKEN}@github.com/celsiusnarhwal/dotfiles
